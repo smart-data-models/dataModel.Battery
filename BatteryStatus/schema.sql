2d4204db1c0a7c71980bbc4023c7eeced1d58b8b
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:54:20 +0100

    test of SQL export

---
+++ BatteryStatus/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model BatteryStatus of the subject dataModel.Battery for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE BatteryStatus_type AS ENUM ('BatteryStatus');
+CREATE TABLE BatteryStatus (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type BatteryStatus_type);
